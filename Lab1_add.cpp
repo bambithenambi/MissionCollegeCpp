ebc542f3ff03b6b1aa58511b8759b7c99394230b
bambithenambi <abhijitvnambiar@gmail.com>
2021-06-09 11:31:34 -0700

    add two numbers

---
+++ Lab1_add.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+    int a;
+    int b;
+    cout << "Enter a number: ";
+    cin >> a;
+    cout << "Enter annother number: ";
+    cin >> b;
+    int sum = a+b;
+    cout << "The sum of your two numbers is: " << sum << endl;
+}
