f324dfa89be7a6bbff9efebf49151d854d405416
bambithenambi <abhijitvnambiar@gmail.com>
2021-06-09 11:13:02 -0700

    init

---
+++ Lab1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x;
+    cout << "Enter a number: ";
+    cin >> x;
+    int d = 2*x;
+    cout << "Here is double your number: " << d << endl;
+}
