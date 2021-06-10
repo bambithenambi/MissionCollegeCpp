db516fae0e3ab3f81fc312b38169dfb494426152
bambithenambi <abhijitvnambiar@gmail.com>
2021-06-10 12:01:25 -0700

    added TrianglePattern

---
+++ TrianglePattern.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+int main() {
+    cout << "   *   " << endl;
+    cout << "\n";
+    cout << "  ***  " << endl;
+    cout << "\n";
+    cout << " ***** " << endl;
+    cout << "\n";
+    cout << "*******" << endl;
+}
