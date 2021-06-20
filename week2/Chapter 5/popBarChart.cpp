16141fea3ff1155614516fc4c6c1297dd023b5e3
bambithenambi <abhijitvnambiar@gmail.com>
2021-06-19 22:50:12 -0700

    forgot to save people.txt lol but fileio works

---
+++ week2/Chapter 5/popBarChart.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    int year;
+    int currentPopulation;
+    int asterisks;
+    int remainder;
+    ifstream inputFile;
+    inputFile.open("People.txt");
+    
+    cout << "\nPRAIRIEVILLE POPULATION GROWTH\n" << endl;
+    cout << "(each * represents 1,000 people)\n" << endl;
+
+    if (inputFile) {
+        while (inputFile >> year) {
+            inputFile >> currentPopulation;
+            remainder = currentPopulation%1000;
+            asterisks = (remainder<500) ? (currentPopulation-remainder)/1000 : (currentPopulation-remainder)/1000+1;
+            cout << year << " ";
+            for (int i=0; i<asterisks; i++) {
+                cout << "*";
+            }
+            cout << "\n" <<endl;
+        }
+    }
+
+
+}
